328b56c46e38f214e37fe621feffa20b88604e32
madmax <maksimkholin@gmail.com>
2024-11-03 19:46:52 +0300

    add: DataBase

---
+++ app/src/main/java/com/example/shoppinglist/data/database/ShopItemEntity.kt
@@ -0,0 +1,14 @@
+package com.example.shoppinglist.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "shop_items")
+data class ShopItemEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val isEnabled: Boolean
+)
